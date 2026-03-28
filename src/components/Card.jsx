f0a8dff56c4540742b59fe4f4ccfcc51c9c61390
chiaracariolato <chiaracariolato@gmail.com>
2026-03-28 16:34:10 +0100

    fix: removed useless comments

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,5 @@
 export default function Card({ language }) {
 
-    // if (!language) {
-    //     return <p>Seleziona un linguaggio</p>;
-    // }
-
     return (
         <div className="card">
             <h3>{language.title}</h3>
